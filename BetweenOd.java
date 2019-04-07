5eb8c1a8d79e5171d2e2b11695cff92de1c0e6ec
Dhandapani299 <49011974+Dhandapani299@users.noreply.github.com>
2019-04-07 00:01:23 -0700

    Add files via upload

---
+++ BetweenOd.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+public class BetweenOd{
+public static void main(String args[])
+{
+Scanner s = new Scanner(System.in);
+		 System.out.println("Enter the number");
+		 int i = s.nextInt();
+		System.out.println("Enter the number");
+		int num2 = s.nextInt(); 
+
+	 
+
+
+do{
+if(i%2!=0){
+
+System.out.println("Result: " +i);
+}	 
+i=i+1;
+}
+while(i<num2);
+ 	
+	}
+}
+
