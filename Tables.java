c984484f1b87e717a048aa333e0c962ee019ccf0
Dhandapani299 <49011974+Dhandapani299@users.noreply.github.com>
2019-04-07 00:03:44 -0700

    Add files via upload

---
+++ Tables.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+public class Tables{
+public static void main(String args[])
+{
+Scanner s = new Scanner(System.in);
+		 System.out.println("Enter the start number");
+		 int num= s.nextInt();
+	 	 int sum= 0;
+  		 
+
+for(int i=1;i<=num;i++){
+sum = i*num;
+System.out.println(sum+" ");
+
+
+}
+
+}
+}
+	 
+
+
+
+
+
